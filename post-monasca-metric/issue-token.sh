2a4cbc48cb6a5a67d4ad7b005b552e3bf8cf75c3
Jim Lin <jimlintw922@gmail.com>
2017-07-27 13:16:01 +0800

    update issue-token.sh

--- post-monasca-metric/issue-token.sh
+++ post-monasca-metric/issue-token.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-token=none
-
-# TODO
-
-echo $token
+export OS_AUTH_URL=http://192.168.210.237:5000/v2.0
+export OS_TENANT_NAME="mini-mon"
+export OS_PROJECT_NAME="mini-mon"
+export OS_USERNAME="monasca-agent"
+export OS_PASSWORD="password"
+export OS_IDENTITY_API_VERSION=2
+openstack token issue
